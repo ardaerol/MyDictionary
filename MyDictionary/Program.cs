fc25d50e66e93eba5125ff7008ca98c3c40881a0
Aziz Arda Erol <20190311036@dogus.edu.tr>
2021-01-21 20:38:29 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> myDictionary = new MyDictionary<string>();
+            
+            myDictionary.Add("Arda");
+            myDictionary.Add("Erol");
+
+            MyDictionary<int> myDictionary1 = new MyDictionary<int>();
+            
+            myDictionary1.Add(3);
+            myDictionary1.Add(23);
+        }
+    }
+}
